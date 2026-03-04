4308f945987971faee88c53b951fa96b497493b2
Shivangi <shivangichaurasia1515@gmail.com>
2026-03-04 21:19:11 +0530

    Updated small fixes

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -25,7 +25,7 @@ setError("")
 
 try{
 
-const res = await fetch(`${API}/register`,{
+const res = await fetch(`${API}/login`,{
 method:"POST",
 headers:{ "Content-Type":"application/json" },
 body:JSON.stringify({email,password})
